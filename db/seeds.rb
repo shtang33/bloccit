944d1c076dba241a16ed14fb23738c300a493b4b
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-03-01 21:39:02 -0500

    Complete Seed Data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,19 @@
-require 'faker'
+ require 'faker'
  
- # Create Posts
+ if Post.find_by(title:"A test Post", body:"blah blah blah") == nil
+   Post.create!(
+   title: "A test Post",
+   body: "blah blah blah" 
+  )
+   p = self
+ end
+
+ if Comment.where(body:"Test comment!").count == 0
+   p.comments.create(body:"Test comment!")
+ end
+
+
+  # Create Posts
  50.times do
    Post.create!(
      title:  Faker::Lorem.sentence,
@@ -16,7 +29,8 @@ require 'faker'
      body: Faker::Lorem.paragraph
    )
  end
- 
+
+ puts "#{Post.last.title}"    
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
