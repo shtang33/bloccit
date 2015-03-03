c44706737f1b0db20199c646b0707b0db3de0ebc
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-03-02 22:02:34 -0500

    Created Posts controller and update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/contact'
+  
+  resources :posts
 
-  get 'welcome/index'
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 
