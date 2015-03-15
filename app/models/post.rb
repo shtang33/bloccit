af119eb3e40710500eb300bb864a95a0e97bfbd5
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-03-15 15:51:17 -0400

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # order all posts by their created_at date, in descending order.
-  default_scope { order('created_at DESC') } #
+  default_scope { order('created_at DESC') }
+
+  scope :ordered_by_title, -> {order('title ASC')}
+  scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
+
 end
