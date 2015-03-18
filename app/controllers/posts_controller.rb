ccebde7fe1f29981dabb51143aebb5d31107a826
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-03-17 21:22:23 -0400

    authorization-assignment-incomplete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
   
 
   def index
-    @posts = Post.all
+    # @posts = Post.all
+    @posts = post_policy_scope(Post)
     authorize @posts
   end
 
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,7 +1,17 @@
 class PostPolicy < ApplicationPolicy
 
-  def index?
-    true
+  class Scope < Scope
+    def index?
+      true
+    end
+
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(:published => true)
+      end
+    end
   end
 
 end
