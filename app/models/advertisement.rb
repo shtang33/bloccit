34e82c5fdc4458bf8d67c0707caa483694dabdb5
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-03-17 21:38:34 -0400

    update and debug

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: advertisements
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  copy       :text
+#  price      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Advertisement < ActiveRecord::Base
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
@@ -5,7 +17,7 @@ class Post < ActiveRecord::Base
   # order all posts by their created_at date, in descending order.
   default_scope { order('created_at DESC') }
 
-  scope :ordered_by_title, -> {order('title ASC')}
-  scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
+  # scope :ordered_by_title, -> {order('title ASC')}
+  # scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
 
 end
