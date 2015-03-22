31dd46b5aa98f0b027b1d714a01fccd36ed96d15
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-03-22 16:08:13 -0400

    topic ans post assignment #2

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,7 @@ require 'faker'
   Post.create!(
     user:   users.sample,
     topic:  topics.sample,
-    # summary: summaries.sample,
+    summary: summaries.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
