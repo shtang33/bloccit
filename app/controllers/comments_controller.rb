0243264d4e8ba93cd8484c4e09a0956202524841
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-04-04 14:13:54 -0400

    destroy-assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,21 +4,19 @@ class CommentsController < ApplicationController
   end
 
   def new
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = Comment.new
     authorize @comment
   end
   
   def create
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
     authorize @comment
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@topic, @post] 
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
       render :new
@@ -30,16 +28,15 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
 
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,12 @@ Rails.application.routes.draw do
   resources :advertisements 
 
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-    end
+    resources :posts, except: [:index]
   end
 
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
 
   get 'about' => 'welcome#about'
 
