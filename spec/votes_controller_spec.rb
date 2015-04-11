de29aec135c121411c8f595621e3d5db4ae1b323
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-04-11 15:34:14 -0400

    mocking-rspec-checkpoint

--- spec/votes_controller_spec.rb
+++ spec/votes_controller_spec.rb
@@ -14,7 +14,7 @@ describe VotesController do
 
   describe '#up_vote' do
     it "adds an up-vote to the post" do
-      # @request.env["HTTP_REFERER"] = '/'
+      request.env["HTTP_REFERER"] = '/'
       @user = authenticated_user
       @post = associated_post
       sign_in @user
