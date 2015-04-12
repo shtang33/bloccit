c6d7ff5fa0a82027eeb9291328b7b4e99d8a125b
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-04-12 15:20:47 -0400

    favoriting-checkpoint

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,10 +5,20 @@ describe User do
   include TestFactories
   
   describe "#favorited(post)" do
-    xit "returns `nil` if the user has not favorited the post" do
+
+    before do
+      @post = associated_post
+      @user = authenticated_user
+      sign_in @user   
+    end
+
+    it "returns `nil` if the user has not favorited the post" do
+      expect(@user.favorited(@post)).to be_nil
   end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.where(post: @post).create
+      expect(@user.favorites.find_by_post_id(@post.id)).to eq(favorite) 
   end
   
   end
