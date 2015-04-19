98b05c4864b0ea84ce0d2410bb8b1faed36bd043
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-04-19 14:00:27 -0400

    public-profile-checkpoint

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -7,7 +7,7 @@ require 'rails_helper'
    before do
      @user = authenticated_user
      @post = associated_post(user: @user)
-     @comment = Comment.new(user: @user, body: "A Comment")
+     @comment = Comment.new(user: @user, post: @post,  body: "A Comment")
      allow(@comment).to receive(:send_favorite_emails)
      @comment.save!
 
