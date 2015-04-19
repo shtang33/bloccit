b80bc6f1321e51aa2297c855caed6e863fdb6b48
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-04-19 16:18:12 -0400

    popular-posts-checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,15 @@ Rails.application.routes.draw do
   get 'comments/edit'
 
   devise_for :users
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
   
   resources :advertisements 
 
   resources :topics do
-    resources :posts, except: [:index]
+    resources :posts, except: [:index], controller: 'topics/posts'
   end
 
-  resources :posts, only: [] do
+  resources :posts, only: [:index] do
     resources :comments, only: [:create, :destroy]
       resources :favorites, only: [:create, :destroy]
 
