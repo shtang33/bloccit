8a6369595c0c75fefbe637cacc91eeb00e31b8e6
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-05-26 22:08:39 -0400

    added blocmetrics javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,29 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+function blocmetrics(name) {
+  
+  var _bm_event = {
+    event: {
+      name: name  
+    }
+  }
+
+  var _bm_request = new XMLHttpRequest();
+  _bm_request.open("POST", "http://localhost:3000/api/events", true);
+  _bm_request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  // #5
+  _bm_request.onreadystatechange = function() {
+
+  };
+  
+  _bm_request.send(jQuery.param(_bm_event));
+}
+
+$('#new_comment').submit(function(ev) {
+    blocmetrics("comment");
+});
+
+  blocmetrics("page view");
+
